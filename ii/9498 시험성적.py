57377cf9003b355d5c69b4b96c21203e6e28b81b
은진 <79642020+abceunjin@users.noreply.github.com>
2021-03-11 19:45:12 +0900

    백준1

---
+++ ii/9498 시험성적.py
@@ -0,0 +1,16 @@
+G = int(input())
+
+if G >= 90:    # 90점 이상
+    print('A')
+
+elif G >= 80:  # 80점 이상
+    print('B')
+
+elif G >= 70:  # 70점 이상
+    print('C')
+
+elif G >= 60:  # 60점 이상
+    print('D')
+
+else:        # 그 이외의 경우
+    print('F')
